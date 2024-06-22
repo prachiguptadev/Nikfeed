55b1493e09d653b25e227a870586afc0a1df9054
Prachi Gupta <117148255+prachiguptadev@users.noreply.github.com>
2024-06-22 12:30:42 +0530

    added simple analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,28 @@
-import UserProvider from './context/user'
-import AllOverlays from "@/app/components/AllOverlays"
-import './globals.css'
-import type { Metadata } from 'next'
+import UserProvider from "./context/user";
+import AllOverlays from "@/app/components/AllOverlays";
+import "./globals.css";
+import type { Metadata } from "next";
+import Script from "next/script";
 
 export const metadata: Metadata = {
-  title: 'e28886-Videofeed',
-  description: 'NikFeed',
-}
+  title: "e28886-Videofeed",
+  description: "NikFeed",
+};
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <UserProvider>
-
-
         <body>
           <AllOverlays />
           {children}
+          <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
         </body>
-
       </UserProvider>
     </html>
-  )
+  );
 }
